7576c80a817f9bee79d84be7a04f4cbc4dd815cb
EzeqJuarez <ezequieledjuarez@gmail.com>
2021-04-01 14:56:50 -0300

    Complete the changes from css to scss

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  home(){
+    this.router.navigateByUrl('home');
   }
 
 }
--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
-  styleUrls: ['./poke-detail.component.css']
+  styleUrls: ['./poke-detail.component.scss']
 })
 export class PokeDetailComponent implements OnInit {
 
